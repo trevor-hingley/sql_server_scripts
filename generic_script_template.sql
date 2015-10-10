a890790f575f5b26ff0eb5e6177b6dfc943e37bf
trevor-hingley <trevor.hingley@oxfordcc.co.uk>
2015-10-10 10:59:24 +0100

    Refactored script.

--- generic_script_template.sql
+++ generic_script_template.sql
@@ -1,35 +1,29 @@
-DECLARE @errorMessage nvarchar(max);
+-- Constants
+DECLARE @database nvarchar(max) = DB_NAME();
+DECLARE @severity int = 15;
+DECLARE @state int = 1;
 
--- Check we are not in a 'system' database
-IF DB_NAME() IN ('master', 'model', 'msdb', 'tempdb')
-BEGIN
-	SET @errorMessage = 'This script should not be run on a system database';
-	GOTO SPEnd;
-END
+-- Safety checks
+IF (@database  IN ('master', 'tempdb', 'model', 'msdb'))
+    GOTO SPError_SystemDatabase;
 
--- Check database version is a compatible level
-DECLARE @compatabilityLevel int;
+IF (@database  LIKE ('ReportServer$%'))
+    GOTO SPError_ReportingDatabase;
 
-SELECT @compatabilityLevel = d.[compatibility_level]
-FROM sys.databases d
-WHERE d.database_id = DB_ID();
-
-IF @compatabilityLevel NOT IN (110)
-BEGIN
-	SET @errorMessage = 'The compatability level of this database (' + @compatabilityLevel + ') is not compatible with this script';
-	GOTO SPEnd;
-END
-
--- Start of script
-
--- SCRIPT GOES HERE!!!
+-- Script Body
+-- <SCRIPT GOES HERE!!!>
 
 -- End of script
+GOTO SPEnd;
+
+SPError_SystemDatabase:
+    RAISERROR('Script should not be run on a system database!', @severity , @state);
+    GOTO SPEnd;
+
+SPError_ReportingDatabase:
+    RAISERROR('Script should not be run on a report server database!', @severity , @state);
+    GOTO SPEnd;
 
 SPEnd:
-
-IF ISNULL(@errorMessage, '') <> ''
-	RAISERROR(@errorMessage, 15, 1);
-
-RETURN;
-
+    PRINT 'Script has completed!';
+	RETURN;
