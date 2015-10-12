c1047967090c26791cc34a7eb8e9fc119b74091c
trevor-hingley <trevor.hingley@oxfordcc.co.uk>
2015-10-12 14:58:25 +0100

    Updated generic script template to check database compatability level (version).

--- generic_script_template.sql
+++ generic_script_template.sql
@@ -1,7 +1,12 @@
--- Constants
-DECLARE @database nvarchar(max) = DB_NAME();
-DECLARE @severity int = 15;
-DECLARE @state int = 1;
+-- Variables / initialisation
+DECLARE @database				nvarchar(max) = DB_NAME();
+DECLARE @compatabilityLevel		int
+DECLARE @severity				int = 15;
+DECLARE @state					int = 1;
+
+SELECT @compatabilityLevel = d.[compatibility_level]
+FROM sys.databases d
+WHERE d.database_id = DB_ID();
 
 -- Safety checks
 IF (@database  IN ('master', 'tempdb', 'model', 'msdb'))
@@ -9,6 +14,9 @@ IF (@database  IN ('master', 'tempdb', 'model', 'msdb'))
 
 IF (@database  LIKE ('ReportServer$%'))
     GOTO SPError_ReportingDatabase;
+
+IF (@compatabilityLevel NOT IN (110))
+	GOTO SPError_DatabaseCompatabilityLevel;
 
 -- Script Body
 -- <SCRIPT GOES HERE!!!>
@@ -23,6 +31,10 @@ SPError_SystemDatabase:
 SPError_ReportingDatabase:
     RAISERROR('Script should not be run on a report server database!', @severity , @state);
     GOTO SPEnd;
+
+SPError_DatabaseCompatabilityLevel:
+	RAISERROR('The database compatalibity level (version) is not compatible with this script!', @severity , @state);
+	GOTO SPEnd;
 
 SPEnd:
     PRINT 'Script has completed!';
