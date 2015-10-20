062423dcb56d90529ebf344eeb4949c90685e245
trevor-hingley <trevor.hingley@oxfordcc.co.uk>
2015-10-20 15:18:40 +0100

    New script to clear data and plan cache for the current database.

---
+++ clear_data_and_plan_cache.sql
@@ -0,0 +1,60 @@
+-- Variables / initialisation
+DECLARE @database			nvarchar(max);
+DECLARE @compatabilityLevel	int
+DECLARE @severity			int;
+DECLARE @state				int;
+
+SELECT @compatabilityLevel = d.[compatibility_level]
+FROM sys.databases d
+WHERE d.database_id = DB_ID();
+
+SET @database = DB_NAME();
+SET @severity = 15;
+SET @state = 1;
+
+-- Safety checks
+IF (@database  IN ('master', 'tempdb', 'model', 'msdb'))
+    GOTO SPError_SystemDatabase;
+
+IF (@database  LIKE ('ReportServer$%'))
+    GOTO SPError_ReportingDatabase;
+
+IF (@compatabilityLevel NOT IN (90, 100, 110))
+	GOTO SPError_DatabaseCompatabilityLevel;
+
+-- ====================================================================================================
+DECLARE @databaseID int;
+
+SET @databaseID = DB_ID();
+
+-- A) CLEAR DATA CACHE
+
+-- This forces all dirty pages for the current database to be written to disk and cleans the buffers.
+CHECKPOINT;
+
+-- This removes all clean buffers from the buffer pool.
+DBCC DROPCLEANBUFFERS WITH NO_INFOMSGS;
+
+-- B) CLEAR THE PLAN CACHE
+
+-- This flushes the plan cache for the database whose id is stored in @databaseID.
+DBCC FLUSHPROCINDB (@databaseID) WITH NO_INFOMSGS;
+-- ====================================================================================================
+
+GOTO SPEnd;
+
+SPError_SystemDatabase:
+    RAISERROR('Script should not be run on a system database!', @severity , @state);
+    GOTO SPEnd;
+
+SPError_ReportingDatabase:
+    RAISERROR('Script should not be run on a report server database!', @severity , @state);
+    GOTO SPEnd;
+
+SPError_DatabaseCompatabilityLevel:
+	RAISERROR('The database compatalibity level (version) is not compatible with this script!', @severity , @state);
+	GOTO SPEnd;
+
+SPEnd:
+    PRINT 'Script has completed!';
+	RETURN;
